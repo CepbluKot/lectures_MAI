38ea891341eb0336ca9758b657b684535311e6a2
Igor <55404866+CepbluKot@users.noreply.github.com>
2021-09-15 11:39:09 +0300

    задание к лекции 2

--- zadanije3.cpp
+++ zadanije3.cpp
@@ -1,53 +1,56 @@
 #include <iostream>
+
 #include <cstring>
+
+#include <string>
+
 using namespace std;
 
-class FooString
-{
+class FooString {
 private:
     char* buf;
 public:
-    FooString(char* tbuf) 
-    {
+    FooString(char* tbuf) {
+
         buf = new char[strlen(tbuf) + 1];
         for (int i = 0; i < strlen(tbuf) + 1; i++) buf[i] = tbuf[i];
-        
-    }
-    ~FooString()
-    {
+
+    }~FooString() {
         delete[] buf;
     }
-    void show()
-    {
-        cout << buf;
+    void show() {
+        std::cout << buf;
     }
-    
-    bool compare(char* str)
-    {
-        
-        if (strlen(buf) != strlen(str)) 
-        {
+
+    bool compare(char* str) {
+
+        if (strlen(buf) != strlen(str)) {
             return false;
         }
-        else 
-        {
+        else {
             for (int i = 0; i < strlen(buf) + 1; i++) {
                 if (buf[i] != str[i]) return false;
             }
         }
         return true;
     }
-
-   
 };
 
+bool testCompare() {
+    char strOne[20] = "word";
+    FooString strTwo(&(strOne[0]));
+    return strTwo.compare(strOne);
+}
 
+void runTest(bool(*testFunction)(),
+    const std::string& testName) {
+    if (testFunction() == 1)
+        std::cout << "Test " << testName << " - OK" << std::endl;
+    else
+        std::cout << "Test " << testName << " - FAIL" << std::endl;
+}
 
-int main()
-{
-   char stri[10] = "abc";
-   char capi[10] = "adc";
-    FooString lol(stri);
-    cout << lol.compare(capi);
+int main() {
+    runTest(testCompare, "testCompare");
     return 0;
 }
