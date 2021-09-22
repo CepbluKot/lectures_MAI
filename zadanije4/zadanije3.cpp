6bab6e20a667762cb995ca52515009d7f3a50800
Igor <55404866+CepbluKot@users.noreply.github.com>
2021-09-22 21:19:16 +0300

    Update zadanije3.cpp

--- zadanije4/zadanije3.cpp
+++ zadanije4/zadanije3.cpp
@@ -1,53 +1,54 @@
 #include <iostream>
-
 #include <stdlib.h>
-
 #include <fstream>
-
 #include <bitset>
-
 #include <string>
-
 #include <sstream>
 
-class Protocol {
-  public:
-
-    virtual void send(char * buf, int len) {};
-
+class Protocol
+{
+public:
+  virtual void send(char *buf, int len){};
 };
 
-class BinaryProtocol: public Protocol {
+class BinaryProtocol : public Protocol
+{
 
-  public: char * buffer;
-  void send(char * buf, int len) {
-    buffer = (char * ) malloc(len * sizeof(char));
+public:
+  char *buffer;
+  void send(char *buf, int len)
+  {
+    buffer = (char *)malloc(len * sizeof(char));
     buffer = buf;
     for (int i = 0; i < len; i++)
-      std::cout << std::bitset < 4 > (buffer[i]).to_string() << " ";
+      std::cout << std::bitset<4>(buffer[i]).to_string() << " ";
   }
 };
 
-class HexProtocol: public Protocol {
-  public: char * buffer;
-  void send(char * buf, int len) {
-    buffer = (char * ) malloc(len * sizeof(char));
+class HexProtocol : public Protocol
+{
+public:
+  char *buffer;
+  void send(char *buf, int len)
+  {
+    buffer = (char *)malloc(len * sizeof(char));
     buffer = buf;
     for (int i = 0; i < len; i++)
       std::cout << "0x" << std::hex << int(buffer[i]) << " ";
   }
-
 };
 
-int main(int argc, char ** argv) {
+int main(int argc, char **argv)
+{
   std::string param = argv[1];
-  std::ifstream in (param);
+  std::ifstream in(param);
   std::string line;
   int count = 0;
-  if ( in .is_open()) {
-    while (getline( in , line)) {
+  if (in.is_open())
+  {
+    while (getline(in, line))
+    {
       // do smth
     }
   }
-
 }
