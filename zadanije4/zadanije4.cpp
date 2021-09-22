b29c2ecacb3a4cf73d56594e8dcbfd02155301f6
Igor <55404866+CepbluKot@users.noreply.github.com>
2021-09-22 20:15:13 +0300

    Update zadanije4.cpp

--- zadanije4/zadanije4.cpp
+++ zadanije4/zadanije4.cpp
@@ -1,52 +1,44 @@
 #include <iostream>
+
 #include <stdlib.h>
+
 #include <fstream>
+
 #include <bitset>
 
 
-class Protocol
-{
-public:
+class Protocol {
+  public:
 
-	virtual void send(char* buf, int len);
+    virtual void send(char * buf, int len) {};
 
 };
 
+class BinaryProtocol: public Protocol {
 
-class BinaryProtocol : public Protocol
-{
-
-public:
-
-
-	void send(char*, int);
+  public: char * buffer;
+  void send(char * buf, int len) {
+    buffer = (char * ) malloc(len * sizeof(char));
+    buffer = buf;
+    for (int i = 0; i < len; i++)
+      std::cout << std::bitset < 4 > (buffer[i]).to_string() << " ";
+  }
 };
 
-class HexProtocol : public Protocol
-{
-public:
+class HexProtocol: public Protocol {
+  public: char * buffer;
+  void send(char * buf, int len) {
+    buffer = (char * ) malloc(len * sizeof(char));
+    buffer = buf;
+    for (int i = 0; i < len; i++)
+      std::cout << "0x" << std::hex << int(buffer[i]) << " ";
+  }
 
-	char* buffer = nullptr;
-	void send(char* buf, int len) {
-		buffer = (char*)malloc(len * sizeof(char));
-		buffer = buf;
-	}
 };
 
-void BinaryProtocol::send(char* buf, int len)
-{
-
-}
-/*
-void HexProtocol::send(char* buf, int len)
-{
-	
-}
-*/
 int main() {
-	HexProtocol heh;
-	char sample[8] = "c";
-	heh.send(sample, 8);
-
+  HexProtocol heh;
+  char sample[15] = "its working";
+  heh.send(sample, sizeof(sample) / sizeof(char));
 
 }
