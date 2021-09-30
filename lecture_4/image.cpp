020c6ffd69a8ada7f2fa6293490e27350f807dff
me <igorcat100@gmail.com>
2021-09-30 23:18:30 +0300

    upd

--- lecture_4/image.cpp
+++ lecture_4/image.cpp
@@ -1,26 +1,49 @@
 #include <iostream>
+#include <exception>
+#include <stdexcept>
 
 class Image {
+	private:
+		
     int * img;
-    int mx, int my;
+    int mx;
+	int my;
+    
     public:
+
         Image(int tmx, int tmy): mx(tmx), my(tmy) 
 		{
             img = new int[mx * my]();
         }
-    vitual~Image();
+        
+    virtual ~Image()
+    {
+    	delete[] img;
+	}
+    
     int get(int x, int y) 
 	{
-        return img[y * my + x];
+		try
+		{	
+        	return img[y * my + x];
+    	}
+    	catch(int error)
+    	{
+    		std::cout<<error;
+		}
     }
+    
     int set(int x, int y, int color) 
 	{
         img[y * my + x] = color;
     }
-    Image operator = (const Image & v)
+    
+    Image operator = (const Image & v);
 };
 
-int maint() 
+
+
+int main() 
 {
 
 }
